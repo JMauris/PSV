4da3081aea95fc5547ea24ece2a3e717791f3560
JMauris <justine.mauris@students.hevs.ch>
2016-11-14 17:37:55 +0100

    setup welcome + add navbar
    logic no-complete

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,1 +1,18 @@
-Hi, <strong><?php echo $username; ?></strong>! You are logged in now. <?php echo anchor('/auth/logout/', 'Logout'); ?>
+<?php include_once('header.php'); ?>
+<html>
+<head>
+  <link rel= "stylesheet" href="<?php echo base_url("dist/css/bootstrap.min.css");?>" />
+	<link rel= "stylesheet" href="<?php echo base_url("dist/css/signin.css");?>" />
+</head>
+<body>
+  <div class="container">
+     <div class="form-signin">
+      <div class="form-group">
+      	<?php echo form_submit('submit_Profil', 'Profil', "class='btn btn-lg btn-primary btn-block'"); ?>
+      	<?php echo form_submit('submit_', 'Prospret', "class='btn btn-lg btn-primary btn-block'"); ?>
+        <?php echo form_submit('submit', 'Aides et Conseils', "class='btn btn-lg btn-primary btn-block'"); ?>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
