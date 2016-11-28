13299cfd33660589cbba2a09bfe805edcea664cf
JMauris <justine.mauris@students.hevs.ch>
2016-11-28 14:34:43 +0100

    PSV fichier SQL

--- application/views/formulaire/module1_form0.php
+++ application/views/formulaire/module1_form0.php
@@ -245,54 +245,50 @@
       </div>
     </div>
   </div>
+	<h1>Lieu</h1>
 	<div class="form-group row">
-	<div class="radio">
+	<div class="col-xs-12">
+		<div class="radio">
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
 	    Salon
 	  </label>
-	</div>
-	<div class="radio">
+
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
 	  Association
 	  </label>
-	</div>
-	<div class="radio">
+
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option2">
 	  Cabaret/Bar
 	  </label>
-	</div>
-	<div class="radio">
+
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios4" value="option2">
 	  Centre RA
 	  </label>
-	</div>
-	<div class="radio">
+
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios5" value="option2">
 	  Loisir/Coiffeur
 	  </label>
-	</div>
-	<div class="radio">
+
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios6" value="option2">
 	  Concert/Fête
 	  </label>
-	</div>
-	<div class="radio">
+
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios7" value="option2">
 	  LRE
 	  </label>
-	</div>
-	<div class="radio">
+
 	  <label>
 	    <input type="radio" name="optionsRadios" id="optionsRadios8" value="option2">
 	    Lieu neutre ( restaurant, salle de cours...)
 	  </label>
+		</div>
 	</div>
 
 
