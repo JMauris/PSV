5e5766efe047ace12fe880544a0ff67fec5f3c04
JMauris <justine.mauris@students.hevs.ch>
2016-12-06 11:38:26 +0100

    ajout date par default

--- application/views/dashBoard/intervention.php
+++ application/views/dashBoard/intervention.php
@@ -98,6 +98,11 @@ foreach ($futur as $key => $value) {
 	<script>
 	$(function() {
 		$(".date").datepicker({ dateFormat: 'dd-mm-yy' });
-
+			var today = new Date();
+			var dd = ("0" + (today.getDate())).slice(-2);
+			var mm = ("0" + (today.getMonth() +　1)).slice(-2);
+			var yyyy = today.getFullYear();
+			today = yyyy + '-' + mm + '-' + dd ;
+			$(".date").attr("value", today);
 	});
 	</script>
